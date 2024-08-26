d44ccc44694c4a96acdee1cf2f2d775e77469a9c
David Cañete <daviddanielc25@gmail.com>
2024-08-26 04:24:49 -0300

    lista incidencias - primer diseño

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 import MapComponent from './components/Map/MapComponent';
-import { MapContainer, TileLayer } from 'react-leaflet';
 
 function App() {
+  const [mapView, setMapView] = React.useState(true);
+
+  const handleChange = (flag) => {
+    return () => setMapView(flag);
+  };
+
   return (
     <>
       <header>
@@ -15,7 +20,8 @@ function App() {
       <div className='divisor-container'>
         <div className='left-menu'>
           <ul>
-            <li>Dashboard</li>
+            <li onClick={handleChange(true)}>Dashboard</li>
+            <li onClick={handleChange(false)}>Incidencias</li>
             <li>Cerrar Sesión</li>
           </ul>
           <button className="cerrar-left-menu">
@@ -23,10 +29,68 @@ function App() {
           </button>
         </div>
         <div className="content">
-          <div className="map-container">
-            <MapComponent />
-          </div>
+          {mapView ? (
+            <div className="map-container">
+              <MapComponent />
+            </div>
+          ) : (
+            <div className="incidencias-container">
+              <div className="campos-list">
+                <div className="incidencia-nombre">
+                  NOMBRE
+                </div>
+                <div className="incidencia-localidad">
+                  LOCALIDAD
+                </div>
+                <div className="incidencia-riesgo">RIESGO</div>
+                <div className="incidencia-estado">ESTADO</div>
+                <div className="incidencia-detalle">
+                </div>
+              </div>
+              <div className="incidencias-list">
+                <div className="incidencia">
+                  <div className="incidencia-nombre">
+                    Incidencia
+                  </div>
+                  <div className="incidencia-localidad">
+                    Jose C Paz
+                  </div>
+                  <div className="incidencia-riesgo">ALTO</div>
+                  <div className="incidencia-estado">En Revisión</div>
+                  <div className="incidencia-detalle">
+                    <div className="boton-detalle">+</div>
+                  </div>
+                </div>
+                <div className="incidencia">
+                  <div className="incidencia-nombre">
+                    Incidencia
+                  </div>
+                  <div className="incidencia-localidad">
+                    Jose C Paz
+                  </div>
+                  <div className="incidencia-riesgo">ALTO</div>
+                  <div className="incidencia-estado">En Revisión</div>
+                  <div className="incidencia-detalle">
+                    <div className="boton-detalle">+</div>
+                  </div>
+                </div>
+                <div className="incidencia">
+                  <div className="incidencia-nombre">
+                    Incidencia
+                  </div>
+                  <div className="incidencia-localidad">
+                    Jose C Paz
+                  </div>
+                  <div className="incidencia-riesgo">ALTO</div>
+                  <div className="incidencia-estado">En Revisión</div>
+                  <div className="incidencia-detalle">
+                    <div className="boton-detalle">+</div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
+          )}
         </div>
       </div>
     </>
