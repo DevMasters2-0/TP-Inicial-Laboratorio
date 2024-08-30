3796ef0c141a9ab350e9d1a8a57380badb74fb7d
David Cañete <daviddanielc25@gmail.com>
2024-08-30 00:15:25 -0300

    script actualizado con npm install

--- script_correr_proyecto.bat
+++ script_correr_proyecto.bat
@@ -4,14 +4,14 @@ title Ejecutar Proyectos Node.js con Vite
 rem Obtiene la ruta del directorio donde está ubicado este script
 set "SCRIPT_DIR=%~dp0"
 
-rem Cambia a la carpeta del proyecto client y ejecuta Vite
-start "Client" cmd /k "cd /d %SCRIPT_DIR%client && npm run dev"
+rem Cambia a la carpeta del proyecto client, ejecuta npm install y luego npm run dev
+start "Client" cmd /k "cd /d %SCRIPT_DIR%client && npm install && npm run dev"
 
-rem Cambia a la carpeta del proyecto admin y ejecuta Vite
-start "Admin" cmd /k "cd /d %SCRIPT_DIR%admin && npm run dev"
+rem Cambia a la carpeta del proyecto admin, ejecuta npm install y luego npm run dev
+start "Admin" cmd /k "cd /d %SCRIPT_DIR%admin && npm install && npm run dev"
 
-rem Cambia a la carpeta del proyecto backend y ejecuta Vite
-start "Backend" cmd /k "cd /d %SCRIPT_DIR%backend && npm run dev"
+rem Cambia a la carpeta del proyecto backend, ejecuta npm install y luego npm run dev
+start "Backend" cmd /k "cd /d %SCRIPT_DIR%backend && npm install && npm run dev"
 
-echo Todos los proyectos se están ejecutando.
+echo Todos los proyectos están siendo instalados y ejecutados.
 pause
