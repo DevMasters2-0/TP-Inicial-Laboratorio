e81d93eaff2c16010b394559bdcc1399f6eb3d20
David Cañete <84222921+imdaviddev@users.noreply.github.com>
2024-08-30 22:23:12 -0300

    fix (#2)

--- backend/src/routes/incidencias.routes.ts
+++ backend/src/routes/incidencias.routes.ts
@@ -8,31 +8,31 @@ import { validateIncidencia } from '../utils/validations/incidencias.validations
 // New Router instance
 const router = Router();
 
+// Valores por defecto que tienen
+router.get('/temas', getTemasController);
+router.get('/riesgos', getNivelesDeRiesgoController);
+router.get('/localidades', getLocalidadesController);
+router.get('/estados', getEstadosController);
+
+// Filtros
+router.get('/temas/:tema', getIncidenciasByTemaController);
+router.get('/riesgos/:nivelDeRiesgo', getIncidenciasByRiesgoController);
+router.get('/localidades/:localidad', getIncidenciasByLocalidadController);
+router.get('/estados/:estado', getIncidenciasByEstadoController);
+
 // Incidencias routes
-router.get('/', getIncidenciasController);
-router.get('/:id', getIncidenciaByIdController);
+router.get('/', getIncidenciasController)
 router.post(
   '/', 
   validateIncidencia, 
   createIncidenciaController 
 );
+router.delete('/', deleteIncidenciaController);
+router.get('/:id', getIncidenciaByIdController);
 router.put(
   '/:id', 
   validateIncidencia,
   updateIncidenciaController
 );
-router.delete('/:id', deleteIncidenciaController);
-
-// Filtros
-router.get('/incidencias/tema/:tema', getIncidenciasByTemaController);
-router.get('/incidencias/riesgo/:nivelDeRiesgo', getIncidenciasByRiesgoController);
-router.get('/incidencias/localidad/:localidad', getIncidenciasByLocalidadController);
-router.get('/incidencias/estado/:estado', getIncidenciasByEstadoController);
-
-// Valores por defecto que tienen
-router.get('/temas', getTemasController);
-router.get('/niveles-de-riesgo', getNivelesDeRiesgoController);
-router.get('/localidades', getLocalidadesController);
-router.get('/estados', getEstadosController);
 
 export default router;
