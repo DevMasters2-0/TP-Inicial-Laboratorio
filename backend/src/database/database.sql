8ce5a9e116b6c1811615c92505082fd1637ddbf9
MAG <gabriellimiguel01@gmail.com>
2024-08-31 18:03:22 -0300

    carga desde la base de datos

--- backend/src/database/database.sql
+++ backend/src/database/database.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS incidencia (
     incidencia_id INTEGER PRIMARY KEY,
+    nombre TEXT,
     dni TEXT,
     email TEXT,
     tema TEXT,
@@ -37,10 +38,10 @@ CREATE TABLE IF NOT EXISTS registro_incidencia (
     FOREIGN KEY(incidencia_id) REFERENCES incidencia(incidencia_id)
 );
 
-INSERT INTO incidencia (incidencia_id, dni, email, tema, nivelDeRiesgo, localidad, descripcion, fechaDeCreacion, latitud, longitud, estado) VALUES
-(1, '12345678A', 'juan.perez@test.com', 'piso roto', 'moderado', 'San Miguel', 'El piso de la cocina está roto y necesita reparación.', '2024-08-31T00:00:00Z', -34.5898, -58.4444, 'En Revisión'),
-(2, '87654321B', 'ana.gomez@test.com', 'calle', 'bajo', 'Polvorines', 'La calle necesita limpieza.', '2024-08-31T00:00:00Z', -34.5900, -58.4450, 'Completado'),
-(3, '13579246C', 'luis.fernandez@test.com', 'alumbrado', 'urgente', 'Jose C Paz', 'El alumbrado público no funciona en la calle principal.', '2024-08-31T00:00:00Z', -34.5902, -58.4460, 'Anulado'),
-(4, '23456789B', 'ana.fernandez@ungs.edu.ar', 'piso roto', 'moderado', 'Polvorines', 'Varios estudiantes y docentes están experimentando tiempos de carga excesivos y errores al intentar acceder a los recursos educativos en la plataforma de e-learning.', '2024-08-31T00:00:00Z', -34.5898, -58.4444, 'En Revisión');
+INSERT INTO incidencia (incidencia_id, nombre, dni, email, tema, nivelDeRiesgo, localidad, descripcion, fechaDeCreacion, latitud, longitud, estado) VALUES
+(1, 'David cañete', '12345678A', 'juan.perez@test.com', 'piso roto', 'moderado', 'San Miguel', 'El piso de la cocina está roto y necesita reparación.', '2024-08-31T00:00:00Z', -34.5898, -58.4444, 'En Revisión'),
+(2, 'German Lucero','87654321B', 'ana.gomez@test.com', 'calle', 'bajo', 'Polvorines', 'La calle necesita limpieza.', '2024-08-31T00:00:00Z', -34.5900, -58.4450, 'Completado'),
+(3, 'Francisco Gottig','13579246C', 'luis.fernandez@test.com', 'alumbrado', 'urgente', 'Jose C Paz', 'El alumbrado público no funciona en la calle principal.', '2024-08-31T00:00:00Z', -34.5902, -58.4460, 'Anulado'),
+(4, 'Ignacio Tula','23456789B', 'ana.fernandez@ungs.edu.ar', 'piso roto', 'moderado', 'Polvorines', 'Varios estudiantes y docentes están experimentando tiempos de carga excesivos y errores al intentar acceder a los recursos educativos en la plataforma de e-learning.', '2024-08-31T00:00:00Z', -34.5898, -58.4444, 'En Revisión');
 
   
