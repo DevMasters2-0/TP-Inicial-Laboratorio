3f06152612bd60780551995e29fbdaf56fdf8726
Ignacio Tula <itula@logos.net.ar>
2024-09-02 04:14:20 -0300

    KAN-55 Método para obtener User por username

--- backend/src/models/users.models.ts
+++ backend/src/models/users.models.ts
@@ -13,59 +13,63 @@ export interface User {
   role: Role; // Campo para el rol del usuario
 }
 
-    
-  const users: Array<User> = [
-    {
-      id: 1,
-      username: 'johndoe',
-      password: '123456',
-      nombre: 'John',
-      apellido: 'Doe',
-      imagenDePerfil: 'https://example.com/profile/johndoe.jpg', // URL de ejemplo
-      fechaDeCreacion: new Date(), // Fecha de creación actual
-      role: Role.ADMIN, // Rol del usuario
-    },
-    {
-      id: 2,
-      username: 'janedoe',
-      password: 'secret',
-      nombre: 'Jane',
-      apellido: 'Doe',
-      imagenDePerfil: 'https://example.com/profile/janedoe.jpg', // URL de ejemplo
-      fechaDeCreacion: new Date(), // Fecha de creación actual
-      role: Role.ADMIN, // Rol del usuario
-    },
-    {
-      id: 3,
-      username: 'jackdoe',
-      password: 'password',
-      nombre: 'Jack',
-      apellido: 'Doe',
-      imagenDePerfil: 'https://example.com/profile/jackdoe.jpg', // URL de ejemplo
-      fechaDeCreacion: new Date(), // Fecha de creación actual
-      role: Role.ADMIN, // Rol del usuario
-    },
-  ];
-  
-  
-  export const getUsers = (): Array<User> => {
-    return users;
-  };
-  
-  export const getUserById = (id: number): User | undefined => {
-    return users.find(user => user.id === id);
-  };
-  
-  export const createUser = (user: User): void => {
-    users.push(user);
-  };
-  
-  export const updateUser = (user: User): void => {
-    const index = users.findIndex(u => u.id === user.id);
-    users[index] = user;
-  };
-  
-  export const deleteUser = (id: number): void => {
-    const index = users.findIndex(u => u.id === id);
-    users.splice(index, 1);
-  };
+
+const users: Array<User> = [
+  {
+    id: 1,
+    username: 'johndoe',
+    password: '123456',
+    nombre: 'John',
+    apellido: 'Doe',
+    imagenDePerfil: 'https://example.com/profile/johndoe.jpg', // URL de ejemplo
+    fechaDeCreacion: new Date(), // Fecha de creación actual
+    role: Role.ADMIN, // Rol del usuario
+  },
+  {
+    id: 2,
+    username: 'janedoe',
+    password: 'secret',
+    nombre: 'Jane',
+    apellido: 'Doe',
+    imagenDePerfil: 'https://example.com/profile/janedoe.jpg', // URL de ejemplo
+    fechaDeCreacion: new Date(), // Fecha de creación actual
+    role: Role.ADMIN, // Rol del usuario
+  },
+  {
+    id: 3,
+    username: 'jackdoe',
+    password: 'password',
+    nombre: 'Jack',
+    apellido: 'Doe',
+    imagenDePerfil: 'https://example.com/profile/jackdoe.jpg', // URL de ejemplo
+    fechaDeCreacion: new Date(), // Fecha de creación actual
+    role: Role.ADMIN, // Rol del usuario
+  },
+];
+
+
+export const getUsers = (): Array<User> => {
+  return users;
+};
+
+export const getUserById = (id: number): User | undefined => {
+  return users.find(user => user.id === id);
+};
+
+export const getUserByUsername = (username: string): User | undefined => {
+  return users.find(user => user.username === username);
+};
+
+export const createUser = (user: User): void => {
+  users.push(user);
+};
+
+export const updateUser = (user: User): void => {
+  const index = users.findIndex(u => u.id === user.id);
+  users[index] = user;
+};
+
+export const deleteUser = (id: number): void => {
+  const index = users.findIndex(u => u.id === id);
+  users.splice(index, 1);
+};
